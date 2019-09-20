b1bb289ee651a738b9c2c58f7c65be83109470a4
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-09-19 17:49:42 -0700

    Updated the expf test

--- software/spmd/expf/main.c
+++ software/spmd/expf/main.c
@@ -1,17 +1,21 @@
 #include <math.h>
 #include <stdio.h>
 
+typedef union {
+  int i;
+  float f;
+} float_or_int_t;
+
 float data = 3.4;
+int expected = 0x41efb67c; // expf(data)
 
 int main() {
-  union {
-    int i;
-    float f;
-  } res;
+  float_or_int_t result;
 
-  res.f = expf(data);
-
-  printf("%x\n", res.i);
+  result.f = expf(data);
+  printf("result=%x expected=%x\n", result.i, expected);  
+  if (result.i != expected) 
+    return -1;
 
   return 0;
 }
