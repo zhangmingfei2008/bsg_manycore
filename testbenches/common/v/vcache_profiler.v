f3db46a68223bfae34701c87cedb177ee32aaec4
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-11-12 22:28:11 -0800

    fixup

--- testbenches/common/v/vcache_profiler.v
+++ testbenches/common/v/vcache_profiler.v
@@ -143,23 +143,23 @@ module vcache_profiler
 
           if(v_v_r) begin
             if(inc_ld) begin
-              $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[34], 0, addr_v_r, data_v_r, "ld");
+              $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[35], 0, addr_v_r, data_v_r, "ld");
             end
             if (inc_st) begin
-              $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[34], 0, addr_v_r, data_v_r, "st");
+              $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[35], 0, addr_v_r, data_v_r, "st");
             end
 
             if(miss) begin
               if(miss_ld)
-                $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[34], 0, addr_v_r, data_v_r, "miss_ld");
+                $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[35], 0, addr_v_r, data_v_r, "miss_ld");
               else if(miss_st)
-                $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[34], 0, addr_v_r, data_v_r, "miss_st");
+                $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[35], 0, addr_v_r, data_v_r, "miss_st");
               else
-                $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[34], 0, addr_v_r, data_v_r, "miss_unk");
+                $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[35], 0, addr_v_r, data_v_r, "miss_unk");
             end
           end
           else
-            $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[34], 0, addr_v_r, data_v_r, "idle");
+            $fwrite(fd2, "%0d,%s,%0d,%0d,0x%0h,%s\n", $time, my_name[35], 0, addr_v_r, data_v_r, "idle");
 
           $fclose(fd2);
         end
